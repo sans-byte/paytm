bc5ab4aebf88f7d84d21f87b7aa7d08c8bc99248
sanskar jain <sanskarj15@gmail.com>
2024-08-05 23:32:14 +0530

    Basic configuration done

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,16 @@
 const express = require("express");
+const app = express();
+const PORT = 3000;
+app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.status(200).send("<h2>Hello world</h2>");
+});
 
+app.listen(PORT, () => {
+  try {
+    console.log(`Listning on port ${PORT}`);
+  } catch (error) {
+    console.log("Error while starting server");
+  }
+});
