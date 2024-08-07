7f00793b818b01451aed1b3630c9b97718995542
sanskar jain <sanskarj15@gmail.com>
2024-08-07 14:09:55 +0530

    frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,23 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { lazy, Suspense } from "react";
+import PageLoading from "./Components/PageLoading";
+const SignUp = lazy(() => import("./pages/SignUp"));
+const Login = lazy(() => import("./pages/Login"));
+const Dashboard = lazy(() => import("./pages/Dashboard"));
 
 function App() {
-
   return (
-    <div>
-        Hello world
-    </div>
-  )
+    <BrowserRouter>
+      <Suspense fallback={<PageLoading />}>
+        <Routes>
+          <Route path="signin" element={<Login />}></Route>
+          <Route path="signup" element={<SignUp />}></Route>
+          <Route path="dashboard" element={<Dashboard />}></Route>
+          <Route path="transfer"></Route>
+        </Routes>
+      </Suspense>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
---
+++ frontend/src/Components/PageLoading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function PageLoading() {
+  return (
+    <div className="justify-center items-center flex h-screen w-screen">
+      <h1>Loading...</h1>
+    </div>
+  );
+}
+
+export default PageLoading;
