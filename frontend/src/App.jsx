e5066864edd8edc1f646bcf6dfce20d898b17472
sanskar jain <sanskarj15@gmail.com>
2024-08-08 14:09:03 +0530

    Added recoil

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { lazy, Suspense } from "react";
 import PageLoading from "./Components/PageLoading";
 import Protected from "./Components/Protected";
+import { RecoilRoot } from "recoil";
 
 const SignUp = lazy(() => import("./pages/SignUp"));
 const Login = lazy(() => import("./pages/Login"));
@@ -11,36 +12,38 @@ const SendMoney = lazy(() => import("./pages/SendMoney"));
 function App() {
   return (
     <BrowserRouter>
-      <Suspense fallback={<PageLoading />}>
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <Protected reverse={true}>
-                <Login />
-              </Protected>
-            }
-          ></Route>
-          <Route
-            path="/signup"
-            element={
-              <Protected reverse={true}>
-                <SignUp />
-              </Protected>
-            }
-          ></Route>
-          <Route
-            path="dashboard"
-            element={
-              <Protected>
-                <Dashboard />
-              </Protected>
-            }
-          ></Route>
-          <Route path="/sendMoney" element={<SendMoney />}></Route>
-          <Route path="/transfer"></Route>
-        </Routes>
-      </Suspense>
+      <RecoilRoot>
+        <Suspense fallback={<PageLoading />}>
+          <Routes>
+            <Route
+              path="/"
+              element={
+                <Protected reverse={true}>
+                  <Login />
+                </Protected>
+              }
+            ></Route>
+            <Route
+              path="/signup"
+              element={
+                <Protected reverse={true}>
+                  <SignUp />
+                </Protected>
+              }
+            ></Route>
+            <Route
+              path="dashboard"
+              element={
+                <Protected>
+                  <Dashboard />
+                </Protected>
+              }
+            ></Route>
+            <Route path="/sendMoney" element={<SendMoney />}></Route>
+            <Route path="/transfer"></Route>
+          </Routes>
+        </Suspense>
+      </RecoilRoot>
     </BrowserRouter>
   );
 }
